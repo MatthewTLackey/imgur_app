172b9b51d1a1e3e4e8c1c574b9430c0d9d94fbb2
MatthewTLackey <lackey.matt@gmail.com>
2013-11-04 17:39:02 -0500

    Refactoring views to use helpers and adding navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Photogur::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  root :to => "pictures#index"
+  
   get 'pictures' => 'pictures#index'
 
   
