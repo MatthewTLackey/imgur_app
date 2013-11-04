de17bd456d0023864f348aba3b8e04427d275b98
MatthewTLackey <lackey.matt@gmail.com>
2013-11-04 16:28:56 -0500

    Moving @pictures in pictures_controller.rb into database

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,68 +1,10 @@
 class PicturesController < ApplicationController
   def index
-    @pictures = [
-      {
-        :title  => "The old church on the coast of White sea",
-        :artist => "Sergey Ershov",
-        :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-      },
-      {
-        :title  => "Sea Power",
-        :artist => "Stephen Scullion",
-        :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
-      },
-      {
-        :title  => "Into the Poppies",
-        :artist => "John Wilhelm",
-        :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
-      }
-    ]
+    @pictures = Picture.all
   end
 
-  # def picture0
-  #   @picture = {
-  #     :title  => "The old church on the cost of White sea",
-  #     :artist => "Sergey Ershov",
-  #     :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-  #   }
-  # end
-
-  # def picture1
-  #   @picture = {
-  #     :title  => "Sea Power",
-  #     :artist => "Stephen Scullion",
-  #     :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
-  #   }
-  # end
-
-  # def picture2
-  #   @picture = {
-  #     :title => "Into the Poppies",
-  #     :artist => "John Wilhelm",
-  #     :url => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
-  #   }
-  # end
-
   def show
-    @pictures = [
-      {
-        :title  => "The old church on the cost of White sea",
-        :artist => "Sergey Ershov",
-        :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-      },  
-      {
-        :title  => "Sea Power",
-        :artist => "Stephen Scullion",
-        :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
-      },
-
-      {
-        :title => "Into the Poppies",
-        :artist => "John Wilhelm",
-        :url => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
-      }
-    ]
-    @picture = @pictures[params[:id].to_i]
+    @picture = Picture.find(params[:id])
   end
 
   def new
@@ -70,6 +12,6 @@ class PicturesController < ApplicationController
 
   def create
     render :text => "Saving a picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
-  
+
   end
 end
